f28801689860c0f657b4f0cc672cb3f4c25afd93
Saiana <111061312+saianatc@users.noreply.github.com>
2022-10-19 01:03:14 +0800

    Delete Task 8.js

--- Task 8.js
+++
@@ -1,7 +0,0 @@
-let food = new Map();
-food.set ("carrot", "cucumber", "tomato");
-food.set ("watermelon", "banana", "orange");
-food.set ("pork", "beef", "chicken");
-for (let name of food) {
-    console.log(`Ключ - ${name[0]}, значение - ${name[1]}`);
-}
